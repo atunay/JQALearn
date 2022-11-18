25cadf583e74c6fefa6d35d1857a117a544417a4
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-11-18 17:55:48 +0200

    added the fifth exercise from the first homework.

---
+++ src/main/java/Fromonetoonehundred.java
@@ -0,0 +1,12 @@
+public class Fromonetoonehundred {
+    public static void main(String[] Args) {
+        int i;
+        int t;
+        for (i = 1; i <= 100; i++) {
+            System.out.println(i);
+                for (t=100; t >= 1; t--)
+            System.out.println(t);
+        }
+
+    }
+}
