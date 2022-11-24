7579c41abe7e8e4e08d7cd26fd7aa37e90352122
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-11-24 17:54:56 +0200

    Exercise two of two!

---
+++ src/main/java/Homework2/ExcTwoOfTwo.java
@@ -0,0 +1,14 @@
+/* Да се състави метод, който приема като аргументи две числа и връща тяхното средноаритметично. */
+
+package Homework2;
+
+public class ExcTwoOfTwo {
+    public static void main(String[] args) {
+        System.out.println(theMethod());
+    }
+    static int theMethod() {
+        int a = 56;
+        int b = 64;
+        return (a + b)/2;
+    }
+}
