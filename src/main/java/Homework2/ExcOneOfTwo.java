dd390849fe46756ca136d3551eb7aa619c3197fc
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-11-24 17:14:26 +0200

    complete the first homework.

---
+++ src/main/java/Homework2/ExcOneOfTwo.java
@@ -0,0 +1,14 @@
+/* Да се състави метод, който приема като аргументи две числа и отпечатва на екрана техния сбор.*/
+
+package Homework2;
+
+public class ExcOneOfTwo {
+    public static void main(String[] args) {
+        theMethod();
+    }
+    static void theMethod () {
+        int a = 13;
+        int b = 13;
+        System.out.println(a + b);
+    }
+}
