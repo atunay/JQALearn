62a592f8d0d0f37146c9fa8c5f9ce30897f9245a
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-11-24 18:10:29 +0200

    Exercise three of two!

---
+++ src/main/java/Homework2/ExcThreeOfTwo.java
@@ -0,0 +1,12 @@
+/*Да се прочете масив от екрана с 5 числа по избор от тип double и да се отпечатат всички резултати*/
+
+package Homework2;
+
+public class ExcThreeOfTwo {
+    public static void main(String[] args) {
+        double[] arr = {2.3, 3.2, 4.3, 7.34, 233.01};
+        int i;
+        for (i=0; i < arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
