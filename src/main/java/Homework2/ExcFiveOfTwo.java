6276ecd70b6d0c94aa3ac9a476078aee2b46e9ff
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-11-25 18:50:09 +0200

    Exercise two complete!

--- src/main/java/Homework2/ExcFiveOfTwo.java
+++ src/main/java/Homework2/ExcFiveOfTwo.java
@@ -14,8 +14,8 @@ public class ExcFiveOfTwo {
         arr2 = arr1; //копирам стойностите във новосъздаденият
         int i; //променлива брояч
         System.out.println("The values of arr2: ");
-        for (i=0; i<arr2.length; ++i) { //обхождам масива и извличам инфо за броя на променливите и техните стойности.
-            System.out.println(arr2[i] + " ");
+        for (i=0; i<arr2.length; i++) { //обхождам масива и извличам инфо за броя на променливите и техните стойности.
+            System.out.println(arr2[i]);
         }
         System.out.print("The number of the variables in arr2 = " + i);
     }
