abfdc6a7c86dad06eb88a3626fc9ef53e423d3ca
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-11-25 09:16:37 +0200

    Exercise four of two!

---
+++ src/main/java/Homework2/ExcFourOfTwo.java
@@ -0,0 +1,19 @@
+/*Да се прочете масив от екрана с 5 числа по избор от тип double и да се отпечатат всички резултати
+
+ */
+
+package Homework2;
+
+import java.util.Arrays;
+
+public class ExcFourOfTwo {
+    public static void main(String[] args) {
+        double [] arr = new double[5];
+        arr [0] = 1.1;
+        arr [1] = 1.2;
+        arr [2] = 1.3;
+        arr [3] = 1.4;
+        arr [4] = 1.5;
+        System.out.println(Arrays.toString(arr));
+    }
+}
