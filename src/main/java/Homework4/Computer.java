451c903e5e45c8e4c6c0a89e2a2083a59c13e14a
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-12-19 19:50:05 +0200

    Exercise four oin the way!

---
+++ src/main/java/Homework4/Computer.java
@@ -0,0 +1,37 @@
+
+/*
+        Създайте клас **Computer** със следните полета от тип:
+        int year,
+        double price,
+        String operationSystem
+
+        Класът Computer да има следния конструктор:
+        - констуктор с параметри **Computer(int year, double price, String operationSystem)**, който да инициализира всички полета със стойностите
+        подадени му като аргументи. С помоща на ключовата дума **THIS**
+
+        В същия клас създайте метод **int comparePrice(Computer compare)**, който сравнява цените на 2 компютъра и връща стойност **(използвайте return)**.
+        Ако цената на първия компютър (този, за който се вика метода) е по-висока от тази на компютъра подаден като аргумент, метода връща -1.
+        Ако цената на компютъра подаден като аргумент (втория компютър) е по-високата, се връща 1, а ако са равни, метода връща стойност 0
+
+        Стойностите/цените на двата компътъра да бъдат изнесени в отделен **клас ComputerPrices с main метод**.
+        Цената на първия компютър да бъде декларирана с помоща на обект от клас **Computer**,
+        а цената на втория компютър да бъде извлечена с подаване на стойности в конструктора на клас Computer - **Computer(int year, double price, String operationSystem)**
+*/
+
+        package Homework4;
+
+public class Computer {
+    int year;
+    double price;
+    String operationSystem;
+
+    Computer(int year, double price, String operationSystem) {
+        this.year = 2000;
+        this.price = 2100.50;
+        this.operationSystem = "Windows";
+    }
+    int comparePrice(Computer compare){
+        if
+        return;
+    }
+}
---
+++ src/main/java/Homework4/ComputerPrices.java
@@ -0,0 +1,9 @@
+package Homework4;
+
+public class ComputerPrices {
+    public static void main(String[] args){
+Computer comp1 = new Computer(2000, 1950.50,"Linux");
+
+
+    }
+}
