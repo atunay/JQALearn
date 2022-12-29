485b03de351352a1fdc41c54d678904b7049bb7b
Tunay Amishev (Tuni) <a.tunay@gmail.com>
2022-12-29 11:58:35 +0200

    Exercise four is on the way!

--- src/main/java/Homework4/Computer.java
+++ src/main/java/Homework4/Computer.java
@@ -6,16 +6,21 @@
         String operationSystem
 
         Класът Computer да има следния конструктор:
-        - констуктор с параметри **Computer(int year, double price, String operationSystem)**, който да инициализира всички полета със стойностите
+        - констуктор с параметри **Computer(int year, double price, String operationSystem)**,
+        който да инициализира всички полета със стойностите
         подадени му като аргументи. С помоща на ключовата дума **THIS**
 
-        В същия клас създайте метод **int comparePrice(Computer compare)**, който сравнява цените на 2 компютъра и връща стойност **(използвайте return)**.
-        Ако цената на първия компютър (този, за който се вика метода) е по-висока от тази на компютъра подаден като аргумент, метода връща -1.
-        Ако цената на компютъра подаден като аргумент (втория компютър) е по-високата, се връща 1, а ако са равни, метода връща стойност 0
+        В същия клас създайте метод **int comparePrice(Computer compare)**,
+        който сравнява цените на 2 компютъра и връща стойност **(използвайте return)**.
+        Ако цената на първия компютър (този, за който се вика метода) е по-висока от тази на компютъра подаден като аргумент,
+        метода връща -1.
+        Ако цената на компютъра подаден като аргумент (втория компютър) е по-високата, се връща 1, а ако са равни,
+        метода връща стойност 0
 
         Стойностите/цените на двата компътъра да бъдат изнесени в отделен **клас ComputerPrices с main метод**.
         Цената на първия компютър да бъде декларирана с помоща на обект от клас **Computer**,
-        а цената на втория компютър да бъде извлечена с подаване на стойности в конструктора на клас Computer - **Computer(int year, double price, String operationSystem)**
+        а цената на втория компютър да бъде извлечена с подаване на стойности в конструктора на клас Computer -
+        **Computer(int year, double price, String operationSystem)**
 */
 
         package Homework4;
@@ -30,13 +35,11 @@ public class Computer {
         this.price = price;
         this.operationSystem = operationSystem;
     }
-    int comparePrice(int x, double y, String z){
-        x = 1988;
-        y = 2000.50;
-        z = "IBM";
-        if (y>price)
-        return 1;
-        if (y<price)
+    int comparePrice(Computer comp1, Computer comp2){
+        if (comp1.price > comp2.price) {
+            return 1;
+        }
+        if (comp1.price < comp2.price)
             return -1;
         else
             return 0;
--- src/main/java/Homework4/ComputerPrices.java
+++ src/main/java/Homework4/ComputerPrices.java
@@ -3,8 +3,12 @@ package Homework4;
 public class ComputerPrices {
     public static void main(String[] args){
 Computer comp1 = new Computer(2000, 1950.50,"Linux");
+Computer comp2 = new Computer(1988, 2000.50, "IBM");
         System.out.println(comp1.year);
         System.out.println(comp1.price);
         System.out.println(comp1.operationSystem);
+        System.out.println(comp2.year);
+        System.out.println(comp2.price);
+        System.out.println(comp2.operationSystem);
     }
 }
